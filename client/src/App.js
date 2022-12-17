017b94488ab4d11c58ab26be932d4967da9c9c13
bmaggiano <brandon.maggiano@gmail.com>
2022-12-16 18:13:57 -0700

    added basic routing for navbar and app.js files

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
+import Home from './pages/home'
+import About from './pages/about'
+import Profile from './pages/profile'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
+import Navbar from './components/navbar.js'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Profile' element={<Profile />} />
+        </Routes>
+      </Router>
+    // </ApolloProvider>
   );
 }
 
---
+++ client/src/components/navbar.js
@@ -0,0 +1,53 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import auth from '../utils/auth'
+
+
+function Navbar() {
+
+    const [currentPage, setCurrentPage] = useState();
+    let location = useLocation();
+
+
+    useEffect(() => {
+        setCurrentPage(location.pathname)
+    }, [location.pathname]);
+
+    return (
+        <>
+        <ul className="nav nav-pills nav-fill">
+            <li className="nav-item">
+                <Link
+                    to={'/'}
+                    onClick={() => setCurrentPage("Home")}
+                    className={currentPage === "/" ? "nav-link active" : "nav-link"}
+                >
+                Home
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to={'/About'}
+                    onClick={() => setCurrentPage("About")}
+                    className={currentPage === "/About" ? "nav-link active" : "nav-link"}
+                >
+                About
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to={'/Profile'}
+                    onClick={() => setCurrentPage("Profile")}
+                    className={currentPage === "/Profile" ? "nav-link active" : "nav-link"}
+                >
+                Profile
+                </Link>
+            </li>
+        </ul>
+        </>
+    )
+}
+
+export default Navbar;
