65d1dd011df6b5d93ee889083971d3aa3806610a
bmaggiano <brandon.maggiano@gmail.com>
2022-12-19 19:49:15 -0700

    added more styling to home page and replaced sunset pic with grass.. also mobile responsiveness

--- client/src/components/loginForm.js
+++ client/src/components/loginForm.js
@@ -4,27 +4,33 @@ function LoginForm() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("")
 
-  return (
-    <div>
-        {/* need onsubmit logic */}
-      <form>
-        <p className="text-white"> username: </p>
-            <input 
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)} 
+    return (
+        <div>
+            {/* need onsubmit logic */}
+            <form>
+                <p className="text-white"> username: </p>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
                 />
-        
-        <p className="text-white"> password: </p>
-            <input 
-            type="text"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            />
 
-      </form>
-    </div>
-  )
+                <p className="text-white"> password: </p>
+                <input
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <br />
+                <button className="loginBtn"
+                    type="submit"
+                    variant="success"
+                > Login
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default LoginForm
--- client/src/components/signupForm.js
+++ client/src/components/signupForm.js
@@ -5,33 +5,39 @@ function SignupForm() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-  return (
-    <div className="d-flex flex-column">
-      <form>
-        <p className="text-white"> email: </p>
-            <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            />
-        
-        <p className="text-white"> username: </p>
-            <input 
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            />
-        
-        <p className="text-white"> password: </p>
-            <input 
-            type="text"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            />
-        
-      </form>
-    </div>
-  )
+    return (
+        <div>
+            <form>
+                <p className="text-white"> email: </p>
+                <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <p className="text-white"> username: </p>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <p className="text-white"> password: </p>
+                <input
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <br />
+                <button className="loginBtn"
+                    type="submit"
+                    variant="success"
+                > Signup
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default SignupForm
