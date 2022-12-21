9e2af9f01f1438de3639d314815b62605327b631
bmaggiano <brandon.maggiano@gmail.com>
2022-12-21 13:42:28 -0700

    added first 3 slider pages

--- client/src/components/carousel.js
+++ client/src/components/carousel.js
@@ -7,6 +7,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // import "swiper/css/pagination";
 import azgolf from '../images/azgolf.png'
 import toptracer from '../images/tracer.webp'
+import profile from '../images/profile.webp'
+import golfbag from '../images/golfbag.jpg'
 
 import "../App.css";
 
@@ -24,16 +26,21 @@ export default function Carousel() {
         style={{
             "--swiper-pagination-color": "darkgreen"
         }}
-        className="h-48 rounded-md"
+        className="py-3 rounded-md"
       >
         <SwiperSlide>
-            <img src={toptracer} className="rounded-md w-fluid"/>
-            <br/>
-            <p className="text-sm mx-2">Hit 5 shots at any driving range with TopTracer to get an average distance for any given club.</p>
-
+        <img src={profile} className="rounded-md w-fluid"/>
+            <h5 className="font-bold pt-2">Step 1: </h5>
+            <p className="text-sm mx-2">Login or create an account. It's literally free and your golf game will thank you one day!</p>
         </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide><img src={golfbag} className="rounded-md w-fluid"/>
+            <h5 className="font-bold pt-2">Step 2: </h5>
+            <p className="text-sm mx-2">Add clubs to your virtual bag.</p></SwiperSlide>
+        <SwiperSlide>
+            <img src={toptracer} className="rounded-md w-fluid"/>
+            <h5 className="font-bold pt-2">Step 3: </h5>
+            <p className="text-sm mx-2">Hit 5 shots at any driving range with TopTracer to get an average distance for any given club.</p>
+        </SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
       </Swiper>
