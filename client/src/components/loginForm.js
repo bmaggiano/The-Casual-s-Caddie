00c06f2098365f3fecef737f7a20ed8cb41cba42
bmaggiano <brandon.maggiano@gmail.com>
2023-01-09 12:12:57 -0700

    changed hover and focus colors for sign up page

--- client/src/components/loginForm.js
+++ client/src/components/loginForm.js
@@ -64,7 +64,7 @@ function LoginForm() {
                 <div>
                   <button
                     type="submit"
-                    className="flex w-full justify-center rounded-md border border-transparent bg-green-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    className="flex w-full justify-center rounded-md border border-transparent bg-green-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
                   >
                     Sign in
                   </button>
